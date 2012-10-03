b20cd2105ddf229dd03745491b6da9af57f286e0
Steve Annessa <steve.annessa@gmail.com>
2012-10-02 23:32:37 -0300

    Remove the duplicate install of ruby
    The install method suggested on rvm's site automatically installs the
    latest ruby version. If you want to run a specific version, like 1.9.3,
    removing --ruby from the install command will stop unnecessary/
    duplicate rubies from being installed which decreases the time spent
    bootstrapping.

--- lib/jobs/bootstrap_ruby.sh
+++ lib/jobs/bootstrap_ruby.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-curl -L https://get.rvm.io | bash -s stable --ruby
+curl -L https://get.rvm.io | bash -s stable
 echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # Load RVM function' >> ~/.bash_profile
 source ~/.bash_profile
 rvm install ruby-1.9.3
